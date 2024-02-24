c1ff798ae926aa9f76e255c94e0f6dcfeb208ecf
Kunologist <jiyikun2002@gmail.com>
2024-02-24 23:38:16 +0800

    [chore] boilerplate to get started

---
+++ main.py
@@ -0,0 +1,15 @@
+#
+#
+#  KookBotX: A khl.py-based micro-framework to build your KOOK bot
+# 
+#  Please check https://github.com/Gennadiyev/KookBotX for license and more.
+# 
+#
+
+from pathlib import Path
+from loguru import logger
+import os
+import asyncio
+from khl import Bot
+
+
